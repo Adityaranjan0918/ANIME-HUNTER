28546ac55f49b986c406432d8b3701157e6309cb
Sajidedix <sajideditz27@gmail.com>
2025-06-20 03:49:52 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -8,12 +8,12 @@ class Config(object):
     TOKEN = "7808483233:AAFDNr1xW2fv112Hik7dM2Bh_wrVdDVktUM"
     mongo_url = "mongodb+srv://hinfi933:4kikA8i7RDzVsB1B@cluster0.xjntjrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://files.catbox.moe/xrdug4.jpg", "https://envs.sh/6O.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    SUPPORT_CHAT = "SANDVILLAGE"
+    UPDATE_CHAT = "Akatsuki_airelin"
+    BOT_USERNAME = "Grabbersvbot"
+    CHARA_CHANNEL_ID = "-1002496865311"
+    api_id = 10658015
+    api_hash = "a0087bca748f86698c53d291c9e5b3af"
 
     
 class Production(Config):
