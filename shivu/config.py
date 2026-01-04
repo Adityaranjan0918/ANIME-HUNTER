fdbb76924808a25dcf3f9695a02454473c7365fc
INDIAN CODER <98512400+Adityaranjan0918@users.noreply.github.com>
2026-01-04 11:56:49 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "7657218453"
-    sudo_users = "7135097212"
-    GROUP_ID = -1002746226173
-    TOKEN = "7808483233:AAFDNr1xW2fv112Hik7dM2Bh_wrVdDVktUM"
+    OWNER_ID = "7299916364"
+    sudo_users = "8502796270"
+    GROUP_ID = "-1002162362171"
+    TOKEN = "8556952711:AAFDnPk4Qy-C5vultYjGacH95lJhlkoUNAE"
     mongo_url = "mongodb+srv://hinfi933:4kikA8i7RDzVsB1B@cluster0.xjntjrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://files.catbox.moe/xrdug4.jpg", "https://envs.sh/6O.jpg"]
-    SUPPORT_CHAT = "SANDVILLAGE"
-    UPDATE_CHAT = "Akatsuki_airelin"
-    BOT_USERNAME = "Grabbersvbot"
-    CHARA_CHANNEL_ID = "-1002496865311"
-    api_id = 10658015
-    api_hash = "a0087bca748f86698c53d291c9e5b3af"
+    SUPPORT_CHAT = "IND_CODER_SUPPORT"
+    UPDATE_CHAT = "THE_IND_CODERS"
+    BOT_USERNAME = "IND_AnimeHunterBot"
+    CHARA_CHANNEL_ID = "-1002191895626"
+    api_id = "24970704"
+    api_hash = "503a6a88cdbca1a6cfec91728c2d7d2b"
 
     
 class Production(Config):
